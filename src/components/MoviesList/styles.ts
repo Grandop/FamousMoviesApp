ebe0067960564c8d64a869de0ae5de4fe41e84cf
Pedro Grando <grandop03@gmail.com>
2023-05-08 13:56:03 -0300

    improving layout

--- src/components/MoviesList/styles.ts
+++ src/components/MoviesList/styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const FlatListCustom = styled.FlatList`
-  margin-top: ${RFValue(20)}px;
+  margin-top: ${RFValue(2)}px;
   margin-left: ${RFValue(20)}px;
   margin-right: ${RFValue(20)}px;
-  margin-bottom: ${RFValue(20)}px;
+  margin-bottom: ${RFValue(2)}px;
 `;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,15 +1,8 @@
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Stacks from "./Stacks";
-import { TrendingInfosContext } from "../contexts/TrendingInfos";
-import Loader from "../components/Loader/Loader";
 
 function Router() {
-  const { loading } = useContext(TrendingInfosContext)
-
-  if(loading) {
-    <Loader/>
-  }
 
   return(
     <NavigationContainer>
